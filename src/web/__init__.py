25c60b99f7bb04e3248e92899ce6648fc98a511e
Albert <adelberteng@gmail.com>
2021-10-20 17:52:49 +0800

    add some new function.

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
-from web.route import index
+from web.route import index 
+from web.route import login
+from web.route import blog
+from web.route import linebot_addfd
+
+methods = ["GET", "POST"]
 
 
 def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", endpoint="index", view_func=index)
+    app.add_url_rule("/login", "login", login, methods=methods)
+    app.add_url_rule("/blog", "blog", blog)
+    app.add_url_rule("/linebot_addfd", "linebot_addfd", linebot_addfd)
 
     return app
--- src/web/route.py
+++ src/web/route.py
@@ -1,14 +1,25 @@
-from flask import render_template
+from flask import render_template, request, jsonify, redirect
 
 def index():
 	return render_template("index.html")
 
 def login():
-	pass
+	if request.method == 'POST':
+		username = request.form.get("username")
+		password = request.form.get("password")
+		print(username)
 
+	elif request.method == 'GET':
+		return render_template("login.html")
+	
 def register():
 	pass
 
 def change_password():
 	pass
 
+def blog():
+	return render_template("blog.html")
+
+def linebot_addfd():
+	return render_template("linebot_addfd.html")
