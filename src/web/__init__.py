179490fa15e1ae87e53cc702fb2665ae4408dc5c
Albert <adelberteng@gmail.com>
2021-10-20 21:06:46 +0800

    add register page.

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 from web.route import index 
 from web.route import login
+from web.route import register
 from web.route import blog
 from web.route import linebot_addfd
 
@@ -12,6 +13,7 @@ def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", endpoint="index", view_func=index)
     app.add_url_rule("/login", "login", login, methods=methods)
+    app.add_url_rule("/register", "register", register, methods=methods)
     app.add_url_rule("/blog", "blog", blog)
     app.add_url_rule("/linebot_addfd", "linebot_addfd", linebot_addfd)
 
--- src/web/route.py
+++ src/web/route.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify, redirect
+from flask import render_template, request, jsonify, redirect, url_for
 
 def index():
 	return render_template("index.html")
@@ -7,13 +7,21 @@ def login():
 	if request.method == 'POST':
 		username = request.form.get("username")
 		password = request.form.get("password")
-		print(username)
+
 
 	elif request.method == 'GET':
 		return render_template("login.html")
 	
 def register():
-	pass
+	if request.method == 'POST':
+		username = request.form.get("username")
+		password = request.form.get("password")
+		password_repeat = request.form.get("password_repeat")
+		if password_repeat != password:
+			return render_template("register.html", not_match_warning = "Two of your passwords does not match, Please retry again.")
+
+	elif request.method == 'GET':
+		return render_template("register.html")
 
 def change_password():
 	pass
