59b3d4187980b90695b41a8160faa55d62cf7296
Albert <adelberteng@gmail.com>
2021-10-21 18:04:56 +0800

    add some text in index page.

--- src/web/route.py
+++ src/web/route.py
@@ -12,24 +12,27 @@ def index():
 def login():
 	form = LoginForm(csrf_enabled=False)
 	if request.method == 'POST':
-		username = request.form.get("username")
-		password = request.form.get("password")
+		username = form.username.data
+		password = form.password.data
 
+		return redirect(url_for("index"))
 
 	elif request.method == 'GET':
-		return render_template("login.html", form=form)
+		return render_template("login.html", form = form)
 	
 def register():
 	if request.method == 'GET':
 		return render_template("register.html")
 	else:
-		username = request.form.get("username")
-		password = request.form.get("password")
+		username = form.username.data
+		password = form.password.data
 		password_repeat = request.form.get("password_repeat")
 		if password_repeat != password:
 			not_match_warning = "Two of your passwords does not match, Please retry again."
 			return render_template("register.html", not_match_warning = not_match_warning)
 		# save user info to db
+
+		return redirect(url_for("login"))
 
 
 def change_password():
