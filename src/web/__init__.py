11770abcd874db29a34a67a138a2b4e7d7fd658a
Albert <adelberteng@gmail.com>
2021-10-21 13:55:29 +0800

    add not_implement page.

--- src/web/__init__.py
+++ src/web/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+# from flask_wtf.csrf import CSRFProtect
+# csrf = CSRFProtect()
 
 from web.route import index 
 from web.route import login
 from web.route import register
+from web.route import not_implement
 from web.route import blog
 from web.route import linebot_addfd
 
@@ -11,9 +14,11 @@ methods = ["GET", "POST"]
 
 def create_app():
     app = Flask(__name__)
+    # csrf.init_app(app)
     app.add_url_rule("/", endpoint="index", view_func=index)
     app.add_url_rule("/login", "login", login, methods=methods)
     app.add_url_rule("/register", "register", register, methods=methods)
+    app.add_url_rule("/not_implement", "not_implement", not_implement)
     app.add_url_rule("/blog", "blog", blog)
     app.add_url_rule("/linebot_addfd", "linebot_addfd", linebot_addfd)
 
--- src/web/route.py
+++ src/web/route.py
@@ -1,27 +1,36 @@
-from flask import render_template, request, jsonify, redirect, url_for
+from flask import render_template
+from flask import request
+from flask import jsonify
+from flask import redirect
+from flask import url_for
+
+from .forms import LoginForm
 
 def index():
 	return render_template("index.html")
 
 def login():
+	form = LoginForm(csrf_enabled=False)
 	if request.method == 'POST':
 		username = request.form.get("username")
 		password = request.form.get("password")
 
 
 	elif request.method == 'GET':
-		return render_template("login.html")
+		return render_template("login.html", form=form)
 	
 def register():
-	if request.method == 'POST':
+	if request.method == 'GET':
+		return render_template("register.html")
+	else:
 		username = request.form.get("username")
 		password = request.form.get("password")
 		password_repeat = request.form.get("password_repeat")
 		if password_repeat != password:
-			return render_template("register.html", not_match_warning = "Two of your passwords does not match, Please retry again.")
+			not_match_warning = "Two of your passwords does not match, Please retry again."
+			return render_template("register.html", not_match_warning = not_match_warning)
+		# save user info to db
 
-	elif request.method == 'GET':
-		return render_template("register.html")
 
 def change_password():
 	pass
@@ -31,3 +40,7 @@ def blog():
 
 def linebot_addfd():
 	return render_template("linebot_addfd.html")
+
+
+def not_implement():
+	return render_template("not_implement.html")
