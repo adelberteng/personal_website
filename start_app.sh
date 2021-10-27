6331fe1d6ef16fb2faf72af57fdfa062a2f26ee7
Albert <adelberteng@gmail.com>
2021-10-27 21:15:29 +0800

    modify start_app

--- start_app.sh
+++ start_app.sh
@@ -2,6 +2,6 @@
 rm ./nginx_conf/nginx.conf
 cp ./nginx_conf/nginx_base.conf ./nginx_conf/nginx.conf
 docker-compose -f ./docker/docker-compose.yaml up -d
-# docker exec -it nginx bash -c 'apt-get update && apt-get install certbot -y && apt-get install python3-certbot-nginx -y'
-# docker exec -it nginx bash -c 'nginx -t && nginx -s reload'
-# docker exec -it nginx bash -c 'certbot --nginx --email adelberteng@gmail.com --agree-tos --no-eff-email -d albertteng.xyz -d www.albertteng.xyz'
+docker exec -it nginx bash -c 'apt-get update && apt-get install certbot -y && apt-get install python3-certbot-nginx -y'
+docker exec -it nginx bash -c 'nginx -t && nginx -s reload'
+docker exec -it nginx bash -c 'certbot --nginx --email adelberteng@gmail.com --agree-tos --no-eff-email -d albertteng.xyz -d www.albertteng.xyz'
