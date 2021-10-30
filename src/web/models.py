280f4d8ae7df69e6afdab8ac8e1be3efb3861d00
Albert <adelberteng@gmail.com>
2021-10-31 00:01:36 +0800

    add more info in about page.

--- src/web/models.py
+++ src/web/models.py
@@ -6,4 +6,4 @@ class User(db.Model):
 	password_hash = db.Column(db.String(128), nullable=False)
 
 	def __repr__(self):
-		return f"id: {self.id} username: {self.username} password_hash: {self.password_hash}"a
+		return f"id: {self.id} username: {self.username} password_hash: {self.password_hash}"
