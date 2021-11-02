8e3254a0cfb2276a0708981757f66519e3c06cbd
Albert <adelberteng@gmail.com>
2021-11-02 10:17:17 +0800

    refine: adjust nginx.conf

--- build_tools/apply_cert.sh
+++ build_tools/apply_cert.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sudo certbot certonly --webroot --webroot-path=$HOME/cert/ \
-	--email adelberteng@gmail.com --agree-tos --no-eff-email \	
-	--staging -d albertteng.xyz -d www.albertteng.xyz
+sudo certbot certonly --webroot --webroot-path=$HOME/cert/ --email adelberteng@gmail.com --agree-tos --no-eff-email --staging -d albertteng.xyz -d www.albertteng.xyz
--- build_tools/deploy_app.sh
+++ build_tools/deploy_app.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 _TOP_DIR_PATH=$(cd "$(dirname "$0")"; cd ..;pwd)
 # python3 ./src/secret_to_config.py
-# rm $_TOP_DIR_PATH/nginx_conf/nginx.conf
-# cp $_TOP_DIR_PATH/nginx_conf/nginx_base.conf $_TOP_DIR_PATH/nginx_conf/nginx.conf
 docker-compose -f $_TOP_DIR_PATH/docker/docker-compose.yaml up -d
-# docker exec -it nginx bash -c 'apt-get update && apt-get install certbot -y && apt-get install python3-certbot-nginx -y'
-# docker exec -it nginx bash -c 'nginx -t && nginx -s reload'
-# docker exec -it nginx bash -c 'certbot --nginx --email adelberteng@gmail.com --agree-tos --no-eff-email -d albertteng.xyz -d www.albertteng.xyz'
