74d2641a94ab4c2826b769c665c2482ce5d49b0c
Albert <adelberteng@gmail.com>
2021-11-11 19:54:28 +0800

    add tcp_nopush on

--- src/web/models.py
+++ src/web/models.py
@@ -2,7 +2,7 @@ from web import db
 
 class User(db.Model):
     __tablename__ = 'user_tbl'
-    id = db.Column(db.Integer, primary_key=True)
+    uid = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
 
