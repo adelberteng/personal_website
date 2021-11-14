ef25f2adca281ce8ce33e617b8960c37e1dbd4da
Albert <adelberteng@gmail.com>
2021-11-14 09:55:32 +0800

    add login required in shop.py

--- src/web/shop.py
+++ src/web/shop.py
@@ -7,18 +7,22 @@ bp = Blueprint("shop", __name__, url_prefix="/shop")
 
 
 @bp.route("/shop", methods=("GET", "POST"))
+@login_required
 def shop():
 	return render_template("shop/shop.html")
 
 
 @bp.route("/merchandise", methods=("GET", "POST"))
+@login_required
 def merchandise():
 	pass
 
 @bp.route("/cart", methods=("GET", "POST"))
+@login_required
 def cart():
 	pass
 
 @bp.route("/order", methods=("GET", "POST"))
+@login_required
 def order():
 	pass
