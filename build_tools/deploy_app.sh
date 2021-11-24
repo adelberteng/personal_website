bb176e839f0f111d262ff1b1d9b406bb791ce0c2
Albert <adelberteng@gmail.com>
2021-11-24 21:18:42 +0800

    don't do init db in deploy.sh

--- build_tools/deploy_app.sh
+++ build_tools/deploy_app.sh
@@ -2,4 +2,4 @@
 _TOP_DIR_PATH=$(cd "$(dirname "$0")"; cd ..;pwd)
 # python3 ./src/secret_to_config.py
 docker-compose -f $_TOP_DIR_PATH/docker/docker-compose.yaml up -d
-python3 $_TOP_DIR_PATH/src/db_init.py
+# python3 $_TOP_DIR_PATH/src/db_init.py
